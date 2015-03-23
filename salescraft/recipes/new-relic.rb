e7ad2ff8fbc9c9b535a254c2a1ef7f5c5b64586b
Vinay Nadig <vinay.nadig@icicletech.com>
2015-03-23 15:20:01 +0530

    [Config] Added multi instance new relic config.

--- salescraft/recipes/new-relic.rb
+++ salescraft/recipes/new-relic.rb
@@ -1,3 +1,5 @@
-template "#{node[:deploy][:salescraft-api][:current_path]}/config/newrelic.yml" do
-  source "newrelic.yml.erb"
+node[:deploy].each do |application, deploy|
+  template "#{node[:deploy][application][:current_path]}/config/newrelic.yml" do
+    source "newrelic.yml.erb"
+  end
 end
