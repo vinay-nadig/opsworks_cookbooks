ed6778590b6a10107d9d1206d39f19c13795346e
Vinay Nadig <vinay.nadig@icicletech.com>
2015-03-23 16:15:45 +0530

    [New Relic] Fixed looping through unnecessary keys in the deploy json.

--- salescraft/recipes/new-relic.rb
+++ salescraft/recipes/new-relic.rb
@@ -1,5 +1,3 @@
-node[:deploy].each do |application, deploy|
-  template "#{node[:deploy][application][:deploy_to]}/shared/config/newrelic.yml" do
-    source "newrelic.yml.erb"
-  end
+template "#{node[:deploy]["salescraft-api"][:deploy_to]}/shared/config/newrelic.yml" do
+  source "newrelic.yml.erb"
 end
