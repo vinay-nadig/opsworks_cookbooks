1ac757b65631a634c0dbd2551901b85bfa8d18cc
Vinay Nadig <vinay.nadig@icicletech.com>
2015-04-27 18:38:43 +0530

    [Sidekiq] Added application name to the sidekiq stop recipe.

--- opsworks-sidekiq/recipes/stop.rb
+++ opsworks-sidekiq/recipes/stop.rb
@@ -1,6 +1,10 @@
 SIDEKIQ_PID = File.expand_path("./tmp/pids/sidekiq.pid", __FILE__)
 
-execute "stop sidekiq" do
-  cwd File.join(deploy[:deploy_to], 'current')
-  command "bundle exec sidekiqctl stop #{SIDEKIQ_PID}"
+node[:deploy].each do |application, deploy|
+  if application == 'salescraft-api'
+    execute "stop sidekiq" do
+      cwd File.join(deploy[:deploy_to], 'current')
+      command "bundle exec sidekiqctl stop #{SIDEKIQ_PID}"
+    end
+  end
 end
