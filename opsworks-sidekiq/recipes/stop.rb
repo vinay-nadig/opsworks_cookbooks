5401b4e4b34135b85e42f7fc0ad8d6963e686687
Vinay Nadig <vinay.nadig@icicletech.com>
2015-04-27 18:27:55 +0530

    [Config] Added path to sidekiq stop executor.

--- opsworks-sidekiq/recipes/stop.rb
+++ opsworks-sidekiq/recipes/stop.rb
@@ -1,5 +1,6 @@
 SIDEKIQ_PID = File.expand_path("./tmp/pids/sidekiq.pid", __FILE__)
 
 execute "stop sidekiq" do
+  cwd File.join(deploy[:deploy_to], 'current')
   command "bundle exec sidekiqctl stop #{SIDEKIQ_PID}"
 end
